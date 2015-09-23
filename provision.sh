50f3ec1e92412a0a34f37be7a17a194e20b8b5ae
Simeon Simeonov <sim@fastignite.com>
2015-09-23 11:32:38 -0400

    Updates provisioning

--- provision.sh
+++ provision.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Updating packages..."
-#sudo apt-get -y update
-
 echo "Installing JDK..."
 sudo apt-get -y install openjdk-7-jdk
 
@@ -13,11 +10,11 @@ echo "Installing SBT..."
 debfile="repo-deb-build-0002.deb"
 wget http://apt.typesafe.com/"$debfile"
 dpkg -i "$debfile"
-apt-get update
+apt-get update -y
 apt-get install -y sbt
 echo "SBT defaults may need to be changed. See http://onor.io/2014/01/03/provisioning-a-vm-with-scala-and-sbt/"
 
 echo "Installing Spark..."
 wget http://d3kbcqa49mib13.cloudfront.net/spark-1.5.0-bin-hadoop2.6.tgz
 tar -zxf spark-1.5.0-bin-hadoop2.6.tgz
-ln -d spark-1.5.0-bin-hadoop2.6 spark
+ln -s spark-1.5.0-bin-hadoop2.6 spark
